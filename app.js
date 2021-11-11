b4990c1563036fa438ffc6b399b54ef57d441d44
Konsta Turunen <konsta.turunen@student.laurea.fi>
2021-11-11 18:03:13 +0200

    YRITETÄÄN SAADA VIETYÄ VARAUKSET SAMALLA SERVERILLE

--- app.js
+++ app.js
@@ -108,4 +108,118 @@ app.use("*", (req, res) => {
   });
 });
 
+
+
+//TÄSTÄ ALASPÄIN LISÄTTY UUTTA TIETOA
+
+// Määritellään Varaus -schema ja siihen tietokentät
+const Varaus = mongoose.model(
+  "Varaukset",
+  {
+    tila: String,
+    varaaja: String,
+    pvm: String,
+    aloitus: String,
+    lopetus: String,
+    kesto: Number
+  },
+  "Pesutupavaraukset"
+);
+
+// LUODAAN SIVUSTOLLE REITIT JA NIIDEN OMINAISUUDET
+
+// Tulostetaan kaikki varaukset.
+app.get("/api/getall", function (req, res) {
+  Varaus.find({}, function (err, results) {
+    console.log("Kaikki liikunta-data on haettu!");
+    res.json(results);
+  });
+});
+
+// Haetaan varaajan perusteella.
+app.get("/api/varaaja/:text", function (req, res) {
+  Varaus.find({ varaaja: req.params.text }, function (err, results) {
+    console.log("Henkilön \"" + req.params.text + "\" varaukset on haettu!");
+    res.json(results);
+  });
+});
+
+// Haetaan tilan perusteella.
+app.get("/api/tila/:text", function (req, res) {
+  Varaus.find({ tila: req.params.text }, function (err, results) {
+    console.log("Henkilön \"" + req.params.text + "\" varaukset on haettu!");
+    res.json(results);
+  });
+});
+
+// Haetaan päivämäärän perusteella perusteella.
+app.get("/api/pvm/:text", function (req, res) {
+  Varaus.find({ pvm: req.params.text }, function (err, results) {
+    console.log("Varaukset \"" + req.params.text + "\"  on haettu!");
+    res.json(results);
+  });
+});
+
+
+// Haetaan Id:n perusteella
+app.get("/api/:id", function (req, res) {
+  Varaus.find({ _id: req.params.id }, function (err, results) {
+    console.log("Varaus " + req.params.id + " on haettu");
+    res.json(results);
+  });
+});
+
+// Varausta muokataan id:n perusteella.
+app.put("/api/update/:id", function (req, res) {
+  Varaus.findByIdAndUpdate(req.params.id, req.body, { new: true },
+    (err, todo) => {
+      // Annetaan palaute päivityksen onnistumisesta.
+      if (err) return res.status(500).send(err);
+      console.log("Seuraavan Id:n tiedot on päivitetty: " + req.params.id);
+      res.send("Seuraavan Id:n tiedot on päivitetty: " + req.params.id);
+    })
+});
+
+// Lisätään tietoa sekä luodaan uusi olio, joka tallennetaan tietokantaan.
+app.post("/api/add", function (req, res) {
+  
+  var newVaraukset = new Varaus({
+    tila: req.body.tila,
+    varaaja: req.body.varaaja,
+    pvm: req.body.pvm,
+    aloitus: req.body.aloitus,
+    lopetus: req.body.lopetus,
+    kesto: req.body.kesto
+  });
+
+  // Tallennetaan olio tietokantaan, sekä annetaan palaute siitä.
+  newVaraukset.save(function (err, result) {
+    if (err) console.log(err);
+    console.log("Seuraava varaus on lisätty onnistuneesti \"" + req.body.pvm + "\"");
+    res.send("Seuraava varaus on lisätty onnistuneesti \"" + req.body.pvm + "\"");
+  });
+});
+
+// Poistetaan Mognoosen "hieno" oletusasetus pois käytöstä.
+mongoose.set('useFindAndModify', false);
+
+
+// Poistetaan varaus id:n perusteella.
+app.delete("/api/delete:id", function (req, res) {
+ 
+  var id = req.params.id;
+  Varaus.findByIdAndDelete(id, function (err, results) {
+    if (err) {
+      console.log(err);
+      res.json("EI POISTETTAVAA", 200);
+    } else if (results == null) {
+      res.json("TAPAHTUI VIRHE", 500);
+    } else {
+      console.log("Seuraavan tapahtuman poisto onnistui " + id);
+      res.json("Seuraavan tapahtuman poisto onnistui " + id);
+    }
+  });
+});
+//TÄSTÄ YLÖSPÄIN LISÄTTY UUTTA TIETOA
+
 module.exports = app;
