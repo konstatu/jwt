5a7381d4732652f5b25288edcc11f7cd5f4d9d58
Konsta Turunen <konsta.turunen@student.laurea.fi>
2021-11-11 18:09:39 +0200

    const mongoose lisäys

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ require("./config/database").connect();
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
 
 const User = require("./model/user");
 const auth = require("./middleware/auth");
